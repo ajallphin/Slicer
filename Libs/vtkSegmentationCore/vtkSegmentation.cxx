ce0c36563f20ddf084e35b28cb8d862e3207050c
James Butler <james.butler@revvity.com>
2022-08-17 17:11:27 -0400

    BUG: Fix segment selection in SonoEQ sequences
    This reverts the main code change in https://github.com/Slicer/Slicer/commit/5d2004b3b106c07acfd8b5e41e4cd8ae9e763321
    
    With Slicer appropriately removing the segments it was resulting in a weird issue of segment selection causing various issues. SonoEQ may very well have a set of weird workaround for previous Slicer sequence issues that needs to be removed to work with Slicer's updated code for removing segments. See https://github.com/PerkinElmer/SonoEQ/pull/6422#issuecomment-1215443367 for the various issues.

--- Libs/vtkSegmentationCore/vtkSegmentation.cxx
+++ Libs/vtkSegmentationCore/vtkSegmentation.cxx
@@ -732,6 +732,8 @@ void vtkSegmentation::RemoveSegment(SegmentMap::iterator segmentIt)
 //---------------------------------------------------------------------------
 void vtkSegmentation::RemoveAllSegments()
 {
+  this->SegmentIds.clear();
+
   std::vector<std::string> segmentIds;
   this->GetSegmentIDs(segmentIds);
   for (std::vector<std::string>::iterator segmentIt = segmentIds.begin(); segmentIt != segmentIds.end(); ++segmentIt)
@@ -739,7 +741,6 @@ void vtkSegmentation::RemoveAllSegments()
     this->RemoveSegment(*segmentIt);
   }
   this->Segments.clear();
-  this->SegmentIds.clear();
 
   this->SegmentIdAutogeneratorIndex = 0;
 }
